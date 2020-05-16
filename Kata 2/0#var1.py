c4d3f1220da2d2a338dd36bdefc634b7be086e94
Carlos Luces <ender.fox3@gmail.com>
2020-05-16 12:05:13 +0200

    kata2

---
+++ Kata 2/0#var1.py
@@ -0,0 +1,10 @@
+"""
+Una panadería vende barras de pan a 3.49€ cada una. El pan que no es del día tiene un descuendo del 60%.
+Escribe un programa que comience leyendo el número de barras vendidas que no son del día. Después tu programa debe mostrar el precio habitual de una barra de pan, el descuento que se le hace por no ser fresca y el coste dinal total.
+"""
+
+precio = 3.49
+descuento=0.4 
+precio_con_descuento = precio*descuento
+
+numero_de_barras = input("Introduce el número de barras vendidas:") #guarda lo que el usuario ingresa en variable
